975de3c9a807d870091bb0ee50f8a85f9a569aed
jose9david1 <jsrosas02@gmail.com>
2023-02-17 19:11:15 -0300

    primer commit

---
+++ Animacion.js
@@ -0,0 +1,8 @@
+
+document.getElementById('boton_mostrar').onclick= function(){
+    document.getElementById('info_mostrable').style = 'display:block'  
+}
+
+document.getElementById('boton_ocultar').onclick= function(){
+    document.getElementById('info_mostrable').style = 'display:none'  
+}
